612a74c89f3e7c385dd320b8484ac19fe5b7c291
shiv kumar <shiv56305@gmail.com>
2021-08-21 21:44:44 +0530

    traversal algo added

--- binaryTree.py
+++ binaryTree.py
@@ -20,6 +20,48 @@ class BinaryTree:
                 self.right=BinaryTree(data)
         else :
             return
+    def in_order_traversal(self):
+        elements=[]
+        if self.left:
+            elements+=self.left.in_order_traversal()
+
+        elements.append(self.data)
+
+        if self.right:
+            elements+=self.right.in_order_traversal()
+
+        return elements
+
+    def pre_order_traversal(self):
+        elements=[]
+
+        elements.append(self.data)
+
+        if self.left:
+            elements+=self.left.pre_order_traversal()
+
+        if self.right:
+            elements+=self.right.pre_order_traversal()
+
+        return elements
+
+
+    def post_order_traversal(self):
+        elements=[]
+
+        
+
+        if self.left:
+            elements+=self.left.post_order_traversal()
+
+        if self.right:
+            elements+=self.right.post_order_traversal()
+
+        elements.append(self.data)
+
+        return elements
+
+    
 
    
     
@@ -37,4 +79,6 @@ if __name__=="__main__":
     elements=[1500,7,12,14,27,900,88,23]
 
     root=build(elements)
-    
+    print(root.in_order_traversal())
+    print(root.pre_order_traversal())
+    print(root.post_order_traversal())
