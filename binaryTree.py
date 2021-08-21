14e928b1980f5a899027d612f8944f75167e1c92
shiv kumar <shiv56305@gmail.com>
2021-08-21 21:48:56 +0530

    sum , Max n Min algo added

--- binaryTree.py
+++ binaryTree.py
@@ -4,8 +4,6 @@ class BinaryTree:
         self.left= None
         self.right=None
 
-
-
     def add_child(self,data):
         if data<self.data:
             if self.left:
@@ -20,6 +18,7 @@ class BinaryTree:
                 self.right=BinaryTree(data)
         else :
             return
+
     def in_order_traversal(self):
         elements=[]
         if self.left:
@@ -34,9 +33,7 @@ class BinaryTree:
 
     def pre_order_traversal(self):
         elements=[]
-
         elements.append(self.data)
-
         if self.left:
             elements+=self.left.pre_order_traversal()
 
@@ -45,12 +42,8 @@ class BinaryTree:
 
         return elements
 
-
     def post_order_traversal(self):
         elements=[]
-
-        
-
         if self.left:
             elements+=self.left.post_order_traversal()
 
@@ -58,13 +51,50 @@ class BinaryTree:
             elements+=self.right.post_order_traversal()
 
         elements.append(self.data)
-
         return elements
 
-    
+    def sum_all(self):
+        sum=0 
+        if self.data:
+            sum=sum+self.data
+        if self.left:
+            sum+=self.left.sum_all()
+        if self.right:
+            sum+=self.right.sum_all()
+        return sum
 
-   
-    
+
+    def max_element(self):
+        max=0
+        left=0
+        right=0
+        if self.data:
+            if self.data>max:
+                max=self.data
+        if self.left:
+            if self.left.max_element()>max:
+                max=self.left.max_element()
+        if self.right:
+            if self.right.max_element()>max:
+                max=self.right.max_element()
+
+        return max
+
+    def min_element(self):
+        min=self.data
+        
+        if self.data:
+            if self.data<min:
+                min=self.data
+        if self.left:
+            if self.left.min_element()<min:
+                min=self.left.min_element()
+        if self.right:
+            if self.right.min_element()<min:
+                min=self.right.min_element()
+
+        return min
+
 
 
 def build(elements):
@@ -82,3 +112,6 @@ if __name__=="__main__":
     print(root.in_order_traversal())
     print(root.pre_order_traversal())
     print(root.post_order_traversal())
+    print(root.sum_all())
+    print(root.max_element())
+    print(root.min_element())
